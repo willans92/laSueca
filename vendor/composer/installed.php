bf83eb85d399ab5f7a8bca2dce44de5531f39fad
Williams92 <willans_92@hotmail.com>
2021-06-03 22:32:01 -0400

    twillio

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'b4ea7b69f12e49b6de4fb0fa2d33a8b024740e49',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'b4ea7b69f12e49b6de4fb0fa2d33a8b024740e49',
+            'dev_requirement' => false,
+        ),
+        'twilio/sdk' => array(
+            'pretty_version' => '6.24.1',
+            'version' => '6.24.1.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../twilio/sdk',
+            'aliases' => array(),
+            'reference' => '3438bcdb42a8fabcfaa081670d5924b868016148',
+            'dev_requirement' => false,
+        ),
+    ),
+);
